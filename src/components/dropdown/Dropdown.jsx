8f6b0ebf68d7c5a55516fd5c1656adf9c94b927e
coderumo <alkayarumeysa55@gmail.com>
2024-07-11 01:19:38 +0300

    bonus additions

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Dropdown.css'
 
-const Dropdown = ({data, setDifficultyChange}) => {
+const Dropdown = ({ data, setDifficultyChange }) => {
   return (
     <div className='dropdown'>
-      <select onChange={e=> setDifficultyChange(e.target.value)} name="" id="">
-        {
-          data.map((dt, i) => (
-            <option value={dt}>{dt}</option>
-          ))
-        }
+      <select onChange={e => setDifficultyChange(e.target.value)} defaultValue="Lütfen zorluk düzeyi seçin">
+        <option value="Lütfen zorluk düzeyi seçin" disabled hidden>
+          Lütfen zorluk düzeyi seçin
+        </option>
+        {data.map((dt, i) => (
+          <option key={i} value={dt}>{dt}</option>
+        ))}
       </select>
     </div>
   )
--- src/pages/introduce/Introduc.jsx
+++ src/pages/introduce/Introduc.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Introduce.css'
 import Dropdown from '../../components/dropdown/Dropdown'
 import logo from '../../images/logo2.png'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Introduc = () => { 
     const difficulty = ["easy", "medium", "hard"]
@@ -13,19 +13,25 @@ const Introduc = () => {
     console.log(difficultyChange, "difficultyChange")
     
     const startQuiz = () => {
-      if(difficultyChange){
+      if (difficultyChange && difficultyChange !== "Lütfen zorluk düzeyi seçin") {
         navigate(`/quiz/${difficultyChange}/${TOTAL_QUESTIONS}`)
       }
     }
+
     return (
-    <div className='introduce'>
-      <div className="introduce-container">
-        <img src={logo} alt="Logo" />
-        <Dropdown data= {difficulty} setDifficultyChange = {setDifficultyChange}/>
-            <div onClick={startQuiz} className='introduce-btn'>Quiz e başla</div>
+      <div className='introduce'>
+        <div className="introduce-container">
+          <img src={logo} alt="Logo" />
+          <Dropdown data={difficulty} setDifficultyChange={setDifficultyChange} />
+          <div 
+            onClick={startQuiz} 
+            className={`introduce-btn ${!difficultyChange || difficultyChange === "Lütfen zorluk düzeyi seçin" ? 'disabled' : ''}`}
+          >
+            Quiz'e başla
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
 }
 
 export default Introduc
